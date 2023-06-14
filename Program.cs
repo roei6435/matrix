c8a35c3776b58af0e3fc1d9ff0563e70b21c0f15
roei6435 <Roei6435@gmail.com>
2023-06-14 13:39:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            importData impda = new importData();
+            Console.WriteLine("application:");
+            Console.WriteLine("task board:");
+            Console.WriteLine("List all users:");
+            var listNamesInSystem = await impda.GetListNamesOfUsers();
+            Console.WriteLine("----------------------------------\n ");
+            Console.WriteLine("Enter your USER ID to receive the tasks for you: ");
+            string userId = Console.ReadLine();
+            var tasksForThisUser = await impda.GetListTodosByUserId(userId);
+            Console.WriteLine("you want read more about this user? enter yes/no");
+            string input = Console.ReadLine();
+            bool readMore = input == "yes" ? true : false;
+            if (readMore)
+            {
+                var details = await impda.getAllDetailsByUserId(userId);
+            }
+            Console.WriteLine("---------------------------------------------\nBlog screen:");
+            Console.WriteLine("all posts:");
+            Console.WriteLine("Enter post id ");
+            string postId = Console.ReadLine();
+            var listOfPosts = await impda.GetListPostsByUserId(postId);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ importData.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace matrix
+{
+    public class importData
+    {
+
+        private string baseUrl = "https://jsonplaceholder.typicode.com/";
+
+        ReadOnlyDictionary<string, string> resources = new ReadOnlyDictionary<string, string>(
+      new Dictionary<string, string>
+      {
+                { "todos","todos"},
+                { "comments", "comments" },
+                { "albums", "albums" },
+                { "photos", "photos" },
+                { "users", "users" }
+      });
+
+        public async Task<string> GetJsonFromUrl(string res, string id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string result = await httpClient.GetStringAsync($"{baseUrl}{res}/{id}");
+                if (result == null)
+                {
+                    throw new Exception("Failed to deserialize JSON to the specified type.");
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"שגיאה: {ex.Message}");
+                throw;
+            }
+        }
+        public async Task<List<string>> GetListNamesOfUsers()
+        {
+            string jsonString = await GetJsonFromUrl("users", "");
+            List<string> usernames = new List<string>();
+
+            try
+            {
+                JArray userArray = JArray.Parse(jsonString);
+
+                foreach (JObject userObject in userArray)
+                {
+                    string name = userObject.Value<string>("name");
+                    Console.WriteLine($"{name}");
+                    usernames.Add(name);
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine($"שגיאה בקריאת JSON: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"שגיאה: {ex.Message}");
+            }
+
+            return usernames;
+        }
+
+        public async Task<List<string>> GetListTodosByUserId(string userId)
+        {
+            string jsonString = await GetJsonFromUrl("todos", $"?userId={userId}");
+            List<string> tasks = new List<string>();
+
+            try
+            {
+                JArray userArray = JArray.Parse(jsonString);
+
+                foreach (JObject userObject in userArray)
+                {
+                    string task = userObject.Value<string>("title");
+                    Console.WriteLine($"{task}");
+                    tasks.Add(task);
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine($"שגיאה בקריאת JSON: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"שגיאה: {ex.Message}");
+            }
+
+            return tasks;
+        }
+
+        public async Task <Dictionary<string, object>> getAllDetailsByUserId(string userId)
+        {
+
+            string jsonString = await GetJsonFromUrl("users", userId);
+            JObject userObject = JObject.Parse(jsonString);
+
+            // יצירת מילון מהפרטים
+            var userDictionary = userObject.ToObject<Dictionary<string, object>>();
+
+            // הדפסת המילון
+            foreach (var kvp in userDictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+            return userDictionary;
+
+        }
+
+        public async Task<Dictionary<string, object>> getAllPostsByUserId(string userId)
+        {
+
+            string jsonString = await GetJsonFromUrl("posts", userId);
+            JObject userObject = JObject.Parse(jsonString);
+
+            // יצירת מילון מהפרטים
+            var userDictionary = userObject.ToObject<Dictionary<string, object>>();
+
+            // הדפסת המילון
+            foreach (var kvp in userDictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+            return userDictionary;
+
+        }
+
+        public async Task<List<string>> GetListPostsByUserId(string postId)
+        {
+            string jsonString = await GetJsonFromUrl("comments", $"?postId={postId}");
+            List<string> tasks = new List<string>();
+
+            try
+            {
+                JArray postArry = JArray.Parse(jsonString);
+
+                foreach (JObject userObject in postArry)
+                {
+                    string name = userObject.Value<string>("name");
+                    string body = userObject.Value<string>("body");
+
+                    Console.WriteLine($"{name} : {body}");
+                }
+            }
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine($"שגיאה בקריאת JSON: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"שגיאה: {ex.Message}");
+            }
+
+            return tasks;
+        }
+    }
+
+    
+}
